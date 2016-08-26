06c95a40cd25c2f27fb65e4d08cbe323e3364dda
Joel Waage <joeltwaage@gmail.com>
2016-08-26 11:34:37 -0700

    add looping through meal array

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,22 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>{{ meal.dish }} and {{ meal.details }} and {{ meal.calories }}</h1>
+      <h1>Today's Food:</h1>
+      <div *ngFor="#meal of meals">
+        <h2>{{ meal.dish }}</h2>
+        <h3>{{ meal.details }}</h3>
+        <h4>{{ meal.calories }}</h4>
+      </div>
     </div>
     `
 })
 export class AppComponent {
-  public meal: Meal;
+  public meals: Meal[];
   constructor(){
-    this.meal = new Meal("Dick's Burger", "Just a test", 550)
+    this.meals = [
+      new Meal("Dick's Burger", "Just a test", 550),
+      new Meal("Pagliacci Slice", "Another test", 355)
+    ];
   }
 }
 export class Meal {
