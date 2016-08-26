c11b0a71cfae8962a2ffa28d60e762eac84d23c2
Joel Waage <joeltwaage@gmail.com>
2016-08-26 11:27:44 -0700

    create meal constructor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from 'angular2/core';
 
+
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Connected</h1>
+      <h1>{{ meal.dish }} and {{ meal.details }} and {{ meal.calories }}</h1>
     </div>
     `
 })
 export class AppComponent {
+  public meal: Meal;
+  constructor(){
+    this.meal = new Meal("Dick's Burger", "Just a test", 550)
+  }
+}
+export class Meal {
+  constructor(public dish: string, public details: string, public calories: number) {
 
+  }
 }
